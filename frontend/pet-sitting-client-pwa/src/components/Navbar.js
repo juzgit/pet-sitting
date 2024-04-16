53b0ec90dac4c4b2cc7f1bb623b08f916e6cd68b
Rameck-Junior Gadziso <rameckjuniorgit@gmail.com>
2024-04-16 16:54:30 +0200

    first commit

---
+++ frontend/pet-sitting-client-pwa/src/components/Navbar.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+
+const Navbar = () => {
+    return (
+        <nav className="Navbar">
+            <ul className="navbar-nav">
+                <li className="nav-item">
+                    <a href="#" className="nav-link">Name-App</a>
+                </li>
+
+                <li className="nav-item">
+                    <a href="#" className="nav-link">Sign-Up</a>
+                </li>
+
+                <li className="nav-item">
+                    <a href="#" className="nav-link">Login</a>
+                </li>
+
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar;
